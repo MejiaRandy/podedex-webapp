e5c9ae681609a130efb9abb105a1c235463cf861
MejiaRandy <randyamejia@hotmail.com>
2024-07-10 23:51:36 -0400

    Update: Whatever

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const { sequelize } = require('./models');
+const pokemonRoutes = require('./routes/pokemonRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/pokemons', pokemonRoutes);
+
+app.get('/', (req, res) => {
+  res.redirect('/pokemons');
+});
+
+sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+});
